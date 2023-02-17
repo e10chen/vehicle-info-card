dec046e49c2209bd624bb11fff08b1b11022cfe4
e10chen <ethanchen229@gmail.com>
2023-02-16 22:33:19 -0500

    added working slots and 2 meme-makers

--- src/vehicle-info-card.js
+++ src/vehicle-info-card.js
@@ -12,6 +12,26 @@ export class VehicleInfoCard extends LitElement {
       }, 
       vehicleDescription: {
         type: String,
+      },
+
+      topText: {
+        type: String, 
+        reflect: true
+      },
+
+      bottomText: {
+        type: String,
+        reflect: true
+      },
+
+      subtitle: {
+        type: String, 
+        reflect: true, 
+      },
+
+      img: {
+        type: String, 
+        reflect: true, 
       }
 
     }
@@ -67,7 +87,13 @@ buttons:focus{
   width: 300px;
 }
 
-  @media (min-width: 500px) and (max-width: 800px) {
+meme-maker {
+  border-radius: 10px;
+  border: solid white;
+  width: 300px;
+}
+
+@media (min-width: 500px) and (max-width: 800px) {
     
     .detailButton {
       display: none;
@@ -86,21 +112,34 @@ buttons:focus{
     super();
     this.modelName = "Toytoa Tacoma";
     this.vehicleDescription = "The image above is a Tacoma pickup truck made by the Japanese automaker Toyota. Toyota has been making the Tacoma pickup truck since 1995. The Tacoma has two engines options: 2.7-liter 4-Cylinder and the 3.5-liter V6. It has a miles per gallon of 20 in the city and 23 on the highway."
-
+    this.topText = "IM A"
+    this.bottomText = "TRRRUCK"
+    this.subtitle = "Ethan Chen"
+    this.img = "https://hips.hearstapps.com/hmg-prod/images/2023-toyota-gr-corolla-111-1648581910.jpg"
   }
 
   render() {
     return html`
-<div class="all">
+  <div class="all">
   <h1 class="title">
   ${this.modelName}
-  </h1>
-    <img class ="picture" src ="${toyotaTacoma}">
+</h1>
+
+<div slot="subtitle"> 
+  <h2>${this.subtitle}</h2> 
+  </div>
+
+  <meme-maker alt = "A Toyota Tacoma" image-url = "${toyotaTacoma}" top-text = "${this.topText}" bottom-text = "${this.topText}"></meme-maker>
   
   <p class ="text">
     ${this.vehicleDescription}
   </p>
-    <slot></slot>
+
+  <div slot="img">
+    <meme-maker image-url = "${this.img}" top-text = "VROOM" bottom-text = "VROOM"></meme-maker>
+  </div>
+  
+
 </div>
     `;
   }
